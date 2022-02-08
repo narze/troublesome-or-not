9bf981d28ed2b95ebd61fe53c7ddae1168fc92be
Manassarn Manoonchai <manassarn@gmail.com>
2022-02-08 22:45:03 +0700

    Render

--- src/App.svelte
+++ src/App.svelte
@@ -7,26 +7,56 @@
   import Menu from "./lib/Menu.svelte"
   import Social from "./lib/Social.svelte"
 
-  const url = "https://single-page-svelte.vercel.app"
-  const title = "Single Page Svelte"
+  const url = "https://troublesome-or-not.vercel.app"
+  const title = "คุณคิดว่า..."
 
-  const menuItems = [{ name: "Github", url: "https://github.com/narze/single-page-svelte" }]
+  // const menuItems = [{ name: "Github", url: "https://github.com/narze/troublesome-or-not" }]
 
-  const description = "Build a single page app with Svelte, quickly."
+  const description = ""
   const imageUrl =
-    "https://raw.githubusercontent.com/narze/timelapse/master/projects/single-page-svelte_home.png"
+    "https://raw.githubusercontent.com/narze/timelapse/master/projects/troublesome-or-not_home.png"
   const gtagId = null
+
+  let x, y, value
+
+  function submitIsTroublesome(e) {
+    const rect = e.target.getBoundingClientRect()
+    x = ((e.clientX - rect.left) / rect.width) * 100
+    y = ((e.clientY - rect.top) / rect.height) * 100
+    value = true
+  }
+
+  function submitIsNotTroublesome(e) {
+    const rect = e.target.getBoundingClientRect()
+    x = ((e.clientX - rect.left) / rect.width) * 100
+    y = ((e.clientY - rect.top) / rect.height) * 100
+    value = false
+  }
 </script>
 
-<Kofi name="narze" label="Support Me" />
-<Menu items={menuItems} />
+<!-- <Kofi name="narze" label="Support Me" /> -->
+<!-- <Menu items={menuItems} /> -->
 <Social {url} {title} />
 <Head {title} {description} {url} {imageUrl} {gtagId} />
 
-<main class="w-full h-screen flex flex-col justify-center items-center">
-  <h1 class="text-6xl text-green-400 flex flex-col">
-    <span>Single</span><span>Page</span><span>Svelte</span>
+<main class="w-full h-screen p-2 flex flex-col items-center">
+  <h1 class="text-6xl flex gap-3 mt-2">
+    สร้างความ<span class="underline text-red-600">เดือดร้อน</span>หรือไม่
   </h1>
+
+  <div class="fixed">x: {x}, y: {y}, value: {value}</div>
+
+  <div class="flex-grow w-full flex">
+    <div class="flex-grow text-center flex flex-col">
+      <p class="underline text-red-600 text-xl mt-4">เดือดร้อน</p>
+      <div on:click={submitIsTroublesome} class="bg-gray-200 flex-grow mt-4" />
+    </div>
+    <span class="w-0.5 bg-black mt-14" />
+    <div class="flex-grow text-center flex flex-col">
+      <p class="underline text-red-600 text-xl mt-4">ไม่เดือดร้อน</p>
+      <div on:click={submitIsNotTroublesome} class="bg-gray-200 flex-grow mt-4" />
+    </div>
+  </div>
 </main>
 
 <style>
